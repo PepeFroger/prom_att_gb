52d400af863ce7113f0c75a110c6c97d830b8f03
Aleksandr Petrov <vigasurt@gmail.com>
2023-12-11 15:18:22 +0300

    Сделал задание номер 2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.Write("Введите значение M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akk(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akk(m-1, 1);
+else return akk(m-1, akk(m, n-1));
+}
+
+Console.Write($"Функция Аккермана: {akk(m, n)} ");
