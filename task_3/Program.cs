bf6bb24324964eb3828b95caced232ea695a60fe
Aleksandr Petrov <vigasurt@gmail.com>
2023-12-11 16:48:16 +0300

    Сделал задание номер 3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel;
+int N = 10;
+Random rnd = new Random();
+int [] mas = new int[N];
+for (int i=0;i<N;i++){
+    mas[i] = rnd.Next(-100,100);
+    Console.Write($"{mas[i]}\t");
+}
+Console.WriteLine();
+void RecurcMas(int[] mas, int i){
+    if(i > 0){
+        Console.Write($"{mas[i]}\t");
+        RecurcMas(mas, i-1);
+    }else{
+        return;
+    }
+}
+RecurcMas(mas,N-1);
