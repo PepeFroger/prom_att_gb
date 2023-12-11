761b6531b24425a29648b2e1c93cdcab00ffe9a4
Aleksandr Petrov <vigasurt@gmail.com>
2023-12-11 14:36:53 +0300

    Сделал задание номер 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+void RecNum(int M,int N){
+    if (M>0 & M<N+1){
+        Console.Write($"{M}\t");
+        RecNum(M+1, N);
+    }
+}
+Console.WriteLine("Введите M:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+RecNum(M,N);
