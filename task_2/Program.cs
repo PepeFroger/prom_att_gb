d44ae8f50f20fbfee2b677154950a955313e626f
Aleksandr Petrov <vigasurt@gmail.com>
2023-12-11 16:47:37 +0300

    Изменил задание номер 2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -5,11 +5,10 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int akk(int m, int n)
-{
+int akk(int m, int n){
 if (m == 0) return n + 1;
-else if (n == 0) return akk(m-1, 1);
-else return akk(m-1, akk(m, n-1));
+    else if (n == 0) return akk(m-1, 1);
+    else return akk(m-1, akk(m, n-1));
 }
 
 Console.Write($"Функция Аккермана: {akk(m, n)} ");
